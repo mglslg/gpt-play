c2181648c87abfa19b3f91fbb8ff6074b0317c71
01420524 <suolongga@sf-express.com>
2023-05-05 18:57:48 +0800

    翻译机器人

--- cmd/g/ds/Role.go
+++ cmd/g/ds/Role.go
@@ -5,6 +5,7 @@ type Role struct {
 	ApplicationId  string   `json:"applicationId"`
 	ChannelIds     []string `json:"channelIds"`
 	ClearDelimiter string   `json:"clearDelimiter"`
+	Temperature    float64  `json:"temperature""`
 	Characters     []struct {
 		Desc string `json:"desc"`
 	} `json:"characters"`
