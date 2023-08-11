75e02571507474f40d3d08be61d7b2fdcccf0e29
01420524 <suolongga@sf-express.com>
2023-08-11 17:55:03 +0800

    调试

--- cmd/main/discord_bot.go
+++ cmd/main/discord_bot.go
@@ -318,6 +318,7 @@ func callOpenAIChat(msgStack *ds.Stack, us *ds.UserSession, resultChannel chan s
 		msg, _ := msgStack.Pop()
 
 		role := "user"
+		g.Logger.Println("Harry>>>", us.UserName, ",", us.UserChannelID)
 		if us.UserId == g.Conf.DiscordBotID {
 			role = "assistant"
 		}
